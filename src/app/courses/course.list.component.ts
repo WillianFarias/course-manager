683c43ebac66030a97613b6c3c9d3706bfbb2446
WillianFarias <williansousafarias@gmail.com>
2020-12-22 14:57:47 -0300

    adicionando o bootstrap ao projeto e fazendo teste basico deste

--- src/app/courses/course.list.component.ts
+++ src/app/courses/course.list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-course-list',
-  template: '<h2> Course List </h2>'
+  templateUrl: './course.list.component.html'
 })
+
 export class CourseListComponent {
 
 }
